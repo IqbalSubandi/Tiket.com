f9c6f9204728ccdf2c5b2ecfcbbfabbf8929e63f
Iqbal <iqbal4692@gmail.com>
2021-08-31 10:39:37 +0700

    add project
    add project for the first time

---
+++ Test Suites/Ignis/End to End.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>End to End</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d371707b-3513-438a-bc7e-16e4da3f5fbc</testSuiteGuid>
+   <testCaseLink>
+      <guid>2a01cf95-5ba1-4133-9f9d-272ebbc65093</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ignis/Login/Login Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4d55205-3efe-4090-82ae-04e26f5b9b8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ignis/Things To Do/Add New Things To Do</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
